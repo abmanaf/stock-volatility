3cb90d6416bd07dfeda48d08278b042034e27dcb
abmanaf <abdulmanafaliu414@gmail.com>
2026-04-17 22:35:03 +0000

    fix(cors): update allowed origins for CORS middleware

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=["http://localhost:3000", "https://stock-return-volatility-prediction.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
